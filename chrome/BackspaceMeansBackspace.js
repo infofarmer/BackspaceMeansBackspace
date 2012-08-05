6a0c095feeb0dbd399b08a2e1f3007a4678ca19c
Chris Dzombak <chris@chrisdzombak.net>
2012-08-04 22:31:01 -0400

    Initial commit of unpacked code from http://www.chromeextensions.org/appearance-functioning/backspacemeansbackspace/

---
+++ chrome/BackspaceMeansBackspace.js
@@ -0,0 +1,7 @@
+window.addEventListener('keydown', function(e) {
+    if (e.keyIdentifier == 'U+0008' || e.keyIdentifier == 'Backspace') {
+        if (e.target == document.body) {
+            e.preventDefault();
+        }
+    }
+}, true);
