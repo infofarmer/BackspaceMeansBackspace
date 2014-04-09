60d5c270b418dea6a9e5f3ea81aaa0ade2982a76
Andrew Pantyukhin <infofarmer@gmail.com>
2014-04-09 14:57:31 +0400

    Convert to a userscript

--- BackspaceMeansBackspace.user.js
+++ BackspaceMeansBackspace.user.js
@@ -1,3 +1,12 @@
+// ==UserScript==
+// @name        Backspace Means Backspace
+// @namespace   https://github.com/infofarmer
+// @version     0.1
+// @description Prevents Back action on Backspace key
+// @include     *
+// @copyright   2014+, nollegcraft/playswithlife
+// ==/UserScript==
+
 window.addEventListener('keydown', function(e) {
     if (e.keyIdentifier == 'U+0008' || e.keyIdentifier == 'Backspace') {
         if (e.target == document.body) {
